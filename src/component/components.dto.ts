13f9ce475c5ad78fe94b4abe9a8d59ebb2621cf2
KrupaliVasant <krupali.vasant@brillio.com>
2021-10-05 14:50:47 +0530

    add component dto file

---
+++ src/component/components.dto.ts
@@ -0,0 +1,10 @@
+export interface ComponentsDTO {
+    itemBomNO: number,
+    componenetNo: number,
+    quantity: number,
+    UOM: string,
+    explosionType: string,
+    relOfCosting: boolean,
+    dayInProgress: number,
+    relOfProd: boolean,
+}
