aa22a9e899e369ec02aaf6fd653abd66e834e86c
KrupaliVasant <krupali.vasant@brillio.com>
2021-10-06 18:11:33 +0530

    fetch component by material id

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory,HttpAdapterHost } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './common/middleware/logger.middleware';
 import { AllExceptionsFilter } from './common/exception/all-exceptions.filter';
-import  utils  from './common/util/utils'
+import utils from './common/util/utils'
 declare const module: any;
 
 async function bootstrap() {
@@ -10,7 +10,8 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
   app.setGlobalPrefix('api');
   app.use(logger);
-  app.enableCors();;
+  app.enableCors();
+  await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
